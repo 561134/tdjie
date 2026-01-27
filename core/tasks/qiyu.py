9784a7f8db92ab900f60e999286661571456f5f0
1134 <27561134@qq.com>
2026-01-27 20:24:02 +0800

    update 优化奇遇任务执行速度

--- core/tasks/qiyu.py
+++ core/tasks/qiyu.py
@@ -36,11 +36,11 @@ class QiyuTask(BaseTask):
         if match_pics(template_path='tdjimages/qiyu_wei.png'):
             print("点击奇遇-危图标")
             click_coord(match_pics(template_path='tdjimages/qiyu_wei.png'),do_click=True)
-            time.sleep(5)
+            time.sleep(3)
         elif match_pics(template_path='tdjimages/qiyu_10.png'):# 奇遇10体力任务
             print("此次打奇遇-10体力任务")
             click_coord(match_pics(template_path='tdjimages/qiyu_10.png'),do_click=True)
-            time.sleep(5)
+            time.sleep(3)
         else:
             print("没有匹配到任何 奇遇-危以及普通任务")
         time.sleep(3)
@@ -52,34 +52,24 @@ class QiyuTask(BaseTask):
         time.sleep(2)
         print("点击奇遇-提示图标")
         click_coord(match_pics(template_path='tdjimages/qiyu_tishi.png'),do_click=True)
-        time.sleep(2)
+        time.sleep(1)
         print("点击奇遇-确认图标")
         click_coord(match_pics(template_path='tdjimages/qiyu_queding.png'),do_click=True)
-        time.sleep(3)
+        time.sleep(2)
         print("点击奇遇-自动战斗图标")
         click_coord(match_pics(template_path='tdjimages/qiyu_zidong.png'),do_click=True)
-        time.sleep(1)
-        click_coord(match_pics(template_path='tdjimages/qiyu_zidong.png'),do_click=True)
-        time.sleep(1)
-        click_coord(match_pics(template_path='tdjimages/qiyu_zidong.png'),do_click=True)
-        time.sleep(3)
-        click_coord(match_pics(template_path='tdjimages/qiyu_zidong.png'),do_click=True)
-        time.sleep(1)
         print("点击奇遇-跳过战斗图标")
         click_coord(match_pics(template_path='tdjimages/qiyu_tiaoguo.png'),do_click=True)
         time.sleep(1)
-        click_coord(match_pics(template_path='tdjimages/qiyu_tiaoguo.png'),do_click=True)
+        click_coord(match_pics(template_path='tdjimages/qiyu_zidong.png'),do_click=True)
         time.sleep(1)
         click_coord(match_pics(template_path='tdjimages/qiyu_tiaoguo.png'),do_click=True)
         time.sleep(1)
         click_coord(match_pics(template_path='tdjimages/qiyu_tiaoguo.png'),do_click=True)
-        time.sleep(2)
-        click_coord(match_pics(template_path='tdjimages/qiyu_tiaoguo.png'),do_click=True)
-        time.sleep(12)
+        time.sleep(8)
         qiyu_count = 0# 记录奇遇次数 达到上限就退出任务
         while not match_pics(template_path='tdjimages/zhaying.png'):
-            click_coord(match_pics(template_path='tdjimages/hj_back.png'),do_click=True)
-            click_coord(match_pics(template_path='tdjimages/hj_x.png'),do_click=True)
+            click_coord(match_pics(template_path='tdjimages/qiyu_tiaoguo.png'),do_click=True)
             print("点击空白固定图标")
             click_coord([(off_qiyu[0] + 100, off_qiyu[1] + 400, 1.0)],do_click=True)
             click_coord([(off_qiyu[0] + 100, off_qiyu[1] + 400, 1.0)],do_click=True)
@@ -88,17 +78,18 @@ class QiyuTask(BaseTask):
             if qiyu_count > 5:
                 print("qiyu_count次数超过5次 停止循环匹配扎营图标")
                 break
-        time.sleep(3)
-        while not match_pics(template_path='tdjimages/qicheng.png'):
-            print("点击扎营图标 回营地界面")
-            click_coord(match_pics(template_path='tdjimages/zhaying.png'),do_click=True)
-            click_coord(match_pics(template_path='tdjimages/hj_x.png'),do_click=True)
-            click_coord(match_pics(template_path='tdjimages/hj_back.png'),do_click=True)
-            time.sleep(5)
             if stop_event.is_set():# ★随时响应“停止”
                 print(">>> 手动停止奇遇任务")
                 return
-        time.sleep(3)
+        time.sleep(2)
+        while not match_pics(template_path='tdjimages/qicheng.png'):
+            print("点击扎营图标 回营地界面")
+            click_coord(match_pics(template_path='tdjimages/zhaying.png'),do_click=True)
+            time.sleep(3)
+            if stop_event.is_set():# ★随时响应“停止”
+                print(">>> 手动停止奇遇任务")
+                return
+        time.sleep(2)
         if match_pics(template_path='tdjimages/qicheng.png'):
             print(">>> 奇遇任务结束 成功返回营地")
 
